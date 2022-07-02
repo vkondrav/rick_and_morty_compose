2c4e5ab5ed57475299129edd3e8364ba39cdf541
vkondrav <kondrav@live.ca>
2022-07-02 14:41:40 -0400

    detekt fix

--- buildSrc/src/main/kotlin/Experimental.kt
+++ buildSrc/src/main/kotlin/Experimental.kt
@@ -1,7 +1,8 @@
 object Experimental {
     const val pager = "com.google.accompanist.pager.ExperimentalPagerApi"
     const val animation = "androidx.compose.animation.ExperimentalAnimationApi"
-    const val navigation = "com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi"
+    const val navigation =
+        "com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi"
     const val material = "androidx.compose.material.ExperimentalMaterialApi"
     const val coroutines = "kotlinx.coroutines.ExperimentalCoroutinesApi"
     const val apollo = "com.apollographql.apollo3.annotations.ApolloExperimental"
